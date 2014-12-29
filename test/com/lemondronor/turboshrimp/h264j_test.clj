acbfbd7c9ac111c3265a9e8f8f471f11a57a64c9
wiseman <jjwiseman@gmail.com>
2014-12-29 10:58:16 -0800

    Added test.  ns cleanup.

--- test/com/lemondronor/turboshrimp/h264j_test.clj
+++ test/com/lemondronor/turboshrimp/h264j_test.clj
@@ -1,11 +1,9 @@
 (ns com.lemondronor.turboshrimp.h264j-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
-            [com.lemondronor.turboshrimp.pave :as pave]
             [com.lemondronor.turboshrimp.h264j :as decode]
-            [com.lemonodor.xio :as xio])
-  (:import [java.awt.image BufferedImage]
-           [javax.imageio ImageIO]))
+            [com.lemondronor.turboshrimp.pave :as pave])
+  (:import (java.awt.image BufferedImage)))
 
 
 (deftest h264-decoder-test
@@ -16,7 +14,8 @@
                     io/input-stream
                     pave/read-frame)
           ^BufferedImage img (decoder frame)]
+      (is (instance? BufferedImage img))
       (is (= 672 (.getWidth img)))
       (is (= 418 (.getHeight img)))
-      ;;(ImageIO/write img "png" (io/file "woo.png"))
+      ;;(ImageIO/write img "png" (io/file "decoded-frame.png"))
       )))
