7841b8d9a667023bac323c031550b6796e8b9629
wiseman <jjwiseman@gmail.com>
2014-12-29 14:02:33 -0800

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/turboshrimp-h264j "0.2.1-SNAPSHOT"
+(defproject com.lemondronor/turboshrimp-h264j "0.2.1"
   :description (str "An AR.Drone video decoder for the turboshrimp library "
                     "that uses the h264j H.264 decoder.")
   :url "https://github.com/wiseman/turboshrimp-h264j"
