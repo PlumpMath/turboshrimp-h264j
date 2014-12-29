3894b3b6e955de9b92877e8f0c206d6e700ebc79
wiseman <jjwiseman@gmail.com>
2014-12-29 13:48:15 -0800

    Fixed typo in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.lemondronor/turboshrimp-h264j "0.2.1-SNAPSHOT"
   :description (str "An AR.Drone video decoder for the turboshrimp library "
-                    "that uses the h264j H.264 decoder."
+                    "that uses the h264j H.264 decoder.")
   :url "https://github.com/wiseman/turboshrimp-h264j"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
