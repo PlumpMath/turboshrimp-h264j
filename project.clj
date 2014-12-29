d5514ca3f2b3429323e69f5eda1d7048c33a58a8
wiseman <jjwiseman@gmail.com>
2014-12-28 23:05:06 -0800

    Deploy to clojars by default.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.lemondronor/turboshrimp-h264j "0.2.0"
+(defproject com.lemondronor/turboshrimp-h264j "0.2.1-SNAPSHOT"
   :description (str "An AR.Drone video deocder for the turboshrimp library "
                     "that uses h264j.")
   :url "https://github.com/wiseman/turboshrimp-h264j"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[com.twilight/h264 "0.0.1"]
                  [org.clojure/clojure "1.6.0"]]
   :profiles {:test
