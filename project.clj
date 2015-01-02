5173d3c3d5a531b5605e64dadb04fea8b0c63e71
wiseman <jjwiseman@gmail.com>
2015-01-01 22:55:47 -0800

    Fixed description.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject com.lemondronor/turboshrimp-h264j "0.2.2-SNAPSHOT"
-  :description (str "An AR.Drone video decoder for the turboshrimp library "
-                    "that uses the h264j H.264 decoder.")
+  :description "An AR.Drone video decoder for the turboshrimp library that uses the h264j H.264 decoder."
   :url "https://github.com/wiseman/turboshrimp-h264j"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
